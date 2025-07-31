ef2930db68767279ca05a1e8b1d4f588a5a77fc1
Abdulhaqq Macud <amacud83289@liceo.edu.ph>
2025-07-31 15:56:45 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+    
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Test Branch");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your Student ID");
+        String id = scanner.nextLine();
+        System.out.println("Please enter your First name");
+        String firstName = scanner.nextLine();
+        System.out.println("Please enter your Last name");
+        String lastName = scanner.nextLine();
+        System.out.println("Please enter your Course");
+        String course = scanner.nextLine();
+        System.out.println("Please enter your Section");
+        String section = scanner.nextLine();
+        System.out.println(" ");
+
+        System.out.println("STUDENT INFORMATION");
+        System.out.println("Student ID:" + " " + id);
+        System.out.println("Student name:" + " " + firstName + " " + lastName);
+        System.out.println("Course:" + " " + course);
+        System.out.println("Section:" + " " + section);
+        System.out.println(" ");
+
+        System.out.println("Please enter your Midterm Exam score");
+        int midtermExamScore = scanner.nextInt();
+        System.out.println("Please enter your Final Exam score");
+        int finalExamScore = scanner.nextInt();
+        System.out.println("Please enter your Project score");
+        int projectScore = scanner.nextInt();
+        System.out.println("Please enter your Attendance score");
+        int attendanceScore = scanner.nextInt();
+        int allOverScore = midtermExamScore + finalExamScore + projectScore + attendanceScore;
+        int averageScore = allOverScore / 4;
+        String remarks = (" ");
+        if (averageScore < 75) {
+            remarks = ("FAILED");
+        } else {
+            remarks = ("PASSED");
+        }
+        System.out.println(" ");
+        
+        System.out.println("STUDENT SCORE");
+        System.out.println("Midterm Exam Score: " + midtermExamScore);
+        System.out.println("Final Exam Score: " + finalExamScore);
+        System.out.println("Project Score: " + projectScore);
+        System.out.println("Attendance Score: " + attendanceScore);
+        System.out.println("Average Score: " + averageScore);
+        System.out.println("Remarks: " + remarks);
     }
 }
