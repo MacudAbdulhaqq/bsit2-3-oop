bd3d0b20e313466c4e12418e17da066c85d37e2d
Abdulhaqq Macud <amacud83289@liceo.edu.ph>
2025-08-01 13:57:51 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,28 @@ import java.util.Scanner;
 public class LabActivity2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1 = 0;
-        int num2 = 0;
-        int num3 = 0;
-        int num4 = 0;
-        int num5 = 0;
+        int[] num = new int[5];
+        int i = 0;
         boolean valid = false;
-
         while (!valid){
             try {
-                System.out.println("Enter number 1");
-                num1 = scanner.nextInt();
-                System.out.println("Enter number 2");
-                num2 = scanner.nextInt();
-                System.out.println("Enter number 3");
-                num3 = scanner.nextInt();
-                System.out.println("Enter number 4");
-                num4 = scanner.nextInt();
-                System.out.println("Enter number 5");
-                num5 = scanner.nextInt();
-                valid = true;
+                for (i = 0; i < 5; i++){
+                System.out.println("Enter number " + (i+1));
+                num[i] = scanner.nextInt();
+                }
             } catch (Exception e) {
                 System.out.println("Please enter an integer");
                 scanner.next();
             }
+            if (i > 4){
+                valid = true;
+            }
         }
-
-        int totalNum = num1 + num2 + num3 + num4 + num5;
-
+        int totalNum = num[0] + num[1] + num[2] + num[3] + num[4];
         System.out.println(" ");
-        System.out.println("Number 1: " + num1);
-        System.out.println("Number 2: " + num2);
-        System.out.println("Number 3: " + num3);
-        System.out.println("Number 4: " + num4);
-        System.out.println("Number 5: " + num5);
+        for (i = 0; i < 5; i++) {
+            System.out.println("Number " + (i+1) + ": " + num[i]);
+        }
         System.out.println(" ");
         System.out.println("Total Number: " + totalNum);
         scanner.close();
