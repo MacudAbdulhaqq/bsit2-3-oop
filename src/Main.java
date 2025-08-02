1ca891c09e3e2c7872a77b39f7a4bf390e29f7c7
Abdulhaqq Macud <amacud83289@liceo.edu.ph>
2025-08-02 12:13:23 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Test Branch");
+public class Book{
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable;
+
+    public Book(String Title, String Author, int Pages){
+        title = Title;
+        author = Author;
+        pages = Pages;
+        isAvailable = true;
+        System.out.println("A new book " + title + " by " + author + " has been added to the library!");
+    }
+
+    public void displayInfo(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + isAvailable);
+    }
+
+    public void borrowBook(){
+        if (!isAvailable){
+            System.out.println("Book is unavailable");
+        } else {
+            isAvailable = false;
+            System.out.println("Book Borrowed");
+        }
+    }
+
+    public void returnBook(){
+        isAvailable = true;
+        System.out.println("Book Returned");
+    }
+
+    public static void main(String[] args){
+        Book book1 = new Book("Book 1", "Me", 100);
+        Book book2 = new Book("Book 2", "Myself", 200);
+        Book book3 = new Book("Book 3", "I", 300);
     }
 }
